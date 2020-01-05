7d3c55f0d3172d0b1d13d4a8e5934218a59884a7
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-06 07:24:44 +0900

    Fix the branch name

--- git_push.sh
+++ git_push.sh
@@ -2,24 +2,26 @@
 
 TIMESTAMP=$(date)
 
-BRANCH_NAME="feature/bump-modules-${TIMESTAMP}"
+BRANCH_NAME="feature/bump-modules-$(date "+%Y%m%d-%H%M%S")"
 
 REQUEST_BODY=$(cat << PULL_REQUEST
 {
   "title": "Auto update at ${TIMESTAMP}",
   "body": "Update the modules at ${TIMESTAMP}",
-  "head": "${TIMESTAMP}",
+  "head": "${BRANCH_NAME}",
   "base": "master"
 }
 PULL_REQUEST
 )
 
 git checkout -b "${BRANCH_NAME}" && \
+git config user.name "${USERNAME}" && \
+git config user.email "${MAIL}" && \
 git add * && \
 git commit -m "Auto update at ${TIMESTAMP}" && \
 git push --set-upstream origin "${BRANCH_NAME}" && \
 curl -X POST \
   --url https://api.github.com/repos/poad/web-terminal/pulls \
   --header 'content-type: application/vnd.github.sailor-v-preview+json' \
-  --cookie logged_in=no \
+  --header "authorization: bearer ${GITHUB_TOKEN}" \
   --data "${REQUEST_BODY}"
