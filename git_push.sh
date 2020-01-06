cb73296a47754b4b8d8de3dd14c9ea4dcd0a59a5
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-06 22:07:06 +0900

    Fix the push failed

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env sh
 
+REPO=web-terminal
+
+USER=$(curl -H "Authorization: token ${TOKEN}" https://api.github.com/user) && \
+USER_ID=$(echo ${USER} | jq ".login") && \
+EMAIL=$(echo ${USER} | jq ".email")
+
+TMP_DIR=$(mktemp -d) && \
+cd ${TMP_DIR}
+
+git clone https://${USER}:${TOKEN}@github.com/${USER}/${REPO} && \
+cd ${REPO}
+
+env bash -x update.sh
+
 TIMESTAMP=$(date)
 
 BRANCH_NAME="feature/bump-modules-$(date "+%Y%m%d-%H%M%S")"
@@ -15,9 +29,9 @@ PULL_REQUEST
 )
 
 git checkout -b "${BRANCH_NAME}" && \
-git config user.name "${USERNAME}" && \
-git config user.email "${MAIL}" && \
 git add * && \
+git config --global user.email "${EMAIL}" && \
+git config --global user.name "${USER_ID}" && \
 git commit -m "Auto update at ${TIMESTAMP}" && \
 git push --set-upstream origin "${BRANCH_NAME}" && \
 curl -X POST \
