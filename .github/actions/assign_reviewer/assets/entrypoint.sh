ffdc0c46416772b1dd09e5f53491e2cdfea65e96
Kenji Saito <ken-yo@mbr.nifty.com>
2020-01-08 07:51:33 +0900

    Debug

--- .github/actions/assign_reviewer/assets/entrypoint.sh
+++ .github/actions/assign_reviewer/assets/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env ash
 
+
+if [ -z ${INPUT_PULL_REQUEST_URL} ]; then
+  echo "URL must not empty"
+  exit -1
+fi
+
+echo $#
+
 if [ $# -ne 0 ]; then
   REVIEWERS=$(echo $@ | sed -e "s/ /,/g")
 else
@@ -14,6 +22,10 @@ REQUEST_BODY=$(cat << PULL_REQUEST
 }
 PULL_REQUEST
 )
+
+echo ${INPUT_PULL_REQUEST_URL}
+
+echo "${REQUEST_BODY}"
 
 curl -X POST \
     --url ${INPUT_PULL_REQUEST_URL}/requested_reviewers \
