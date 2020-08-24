dabca813def6cff3b468ca2c4e1b89ed2ee625ea
Kenji Saito <ken-yo@mbr.nifty.com>
2020-08-24 22:59:20 +0900

    node moduleのアップデート方法を変更 (#190)

--- update.sh
+++ update.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env sh
 
+apt-get update -qq && \
+apt-get install -qqy --no-install-recommends \
+  jq
+
 CUR=$(pwd)
 
 for target in $(ls -d xterm*)
 do
   cd ${CUR}/${target}
-  yarn upgrade
+    # parse package.json
+  dev_modules=$(echo -n $(cat package.json | jq -r ".devDependencies | to_entries | .[].key"))
+  echo ${dev_modules}
+
+  modules=$(echo -n $(cat package.json | jq -r ".dependencies | to_entries | .[].key"))
+  echo ${modules}
+  yarn add --dev ${dev_modules}
+  yarn add ${modules}  yarn upgrade
+
   yarn install
   cd assets/webshell
   yarn upgrade
